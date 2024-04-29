87fe618e1fe1462594d82afe72dcd1480cc04e95
dtuneev <dtuneev@gmail.com>
2024-04-29 13:20:51 +0300

    working on data table

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -34,9 +34,8 @@ const buttonVariants = cva(
   }
 )
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
--- src/routes/data-table.lazy.tsx
+++ src/routes/data-table.lazy.tsx
@@ -2,6 +2,7 @@ import { createLazyFileRoute } from '@tanstack/react-router'
 import ContentWrapper from '../components/content-wrapper'
 import { Item, columns } from "../components/tables/columns"
 import { DataTable } from "../components/tables/data-table"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export const Route = createLazyFileRoute('/data-table')({
   component: Index,
@@ -33,6 +34,14 @@ function Index() {
   return (
     <ContentWrapper title="Data table!!!">
       <div className="container mx-auto py-10">
+        Try to play with variants
+        <Tabs defaultValue="outlined" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="account">Outlined</TabsTrigger>
+            <TabsTrigger value="filled">Filled</TabsTrigger>
+            <TabsTrigger value="colorful">Colorful</TabsTrigger>
+          </TabsList>
+        </Tabs>
         <DataTable columns={columns} data={data} />
       </div>
     </ContentWrapper>
